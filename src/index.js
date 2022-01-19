29d7477f619109edf86c328dfdcd8ddfb280e31d
Larasantos97 <larissa_reis97@hotmail.com>
2022-01-19 15:19:56 -0300

    commit primeiras alterações

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 import cipher from './cipher.js';
 
+function button1 buto(){
+const button1 = document.getElementById("button1");
+ button1.addEventListener("click", cifrar);
+
+ const button2 = document.getElementById("button2");
+ button2.addEventListener("click", decifrar);
+}
+
+ function cifrar (){
+ const cifrar = document.getElementById("cifrar")
+ cifrar.addEventListener("Resultado")
+ }
+
+ function decifrar () {
+
+ const decifrar = document.getElementById("decifrar")
+ decifrar.addEventListener("Resultado")
+ }
+
+ function Resultado1 Resultado2() {
+
+ const Resultado1 = document.getElementById("Resultado")
+ Resultado1.innerHTML = cifrar.value
+
+ const Resultado2 = document.getElementById("Resultado")
+ Resultado2.innerHTML = decifrar.value
+ }
+ 
+
+
+
+
+
+
+
+
+ 
 console.log(cipher);
