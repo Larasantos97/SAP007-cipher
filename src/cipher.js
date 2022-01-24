f93fefeb389825cc752f3b8a4703ef569e2f529a
Larasantos97 <larissa_reis97@hotmail.com>
2022-01-24 19:41:46 -0300

    Novas alterações

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,17 @@
 const cipher = {
 
+ //((códigoDaletra + desloc) % tamDoAlfabeto) = (códigoDaletra + 1) % 26
+    //((códigoDaletraASC - cod1aletra + desloc) % tamDoAlfabeto) + cod1aletra
+
+    //function cifra(string){
+        //return
+  //  }
+
+//function encode 
+   
+        
+        
+        
  
 };
 
--- src/index.js
+++ src/index.js
@@ -1,32 +1,42 @@
 import cipher from './cipher.js';
-
-function button1 buto(){
+ 
 const button1 = document.getElementById("button1");
  button1.addEventListener("click", cifrar);
 
  const button2 = document.getElementById("button2");
  button2.addEventListener("click", decifrar);
-}
+
+//const cifra.innerHTML = Resultado1.value
 
  function cifrar (){
- const cifrar = document.getElementById("cifrar")
- cifrar.addEventListener("Resultado")
- }
+ cifra = document.getElementById("cifrar")
+ result = document.getElementById("Resultado1")
+// Resultado1.innerHTML = "cifrar".value
+ return result.value = cipher.encode(cifra,offset);
+
+ //offset = Number(document.getElementById("deslocamento").value
+ 
+}
 
  function decifrar () {
+    decifra = document.getElementById("decifrar")
+    result = document.getElementById("Resultado2")
+    return result.value = cipher.decode(decifra,offset);
 
- const decifrar = document.getElementById("decifrar")
- decifrar.addEventListener("Resultado")
+    //offset = Number(document.getElementById("deslocamento").value
+    
+    
  }
 
- function Resultado1 Resultado2() {
 
- const Resultado1 = document.getElementById("Resultado")
- Resultado1.innerHTML = cifrar.value
+ //*function Resultado1, Resultado2(string) {
 
- const Resultado2 = document.getElementById("Resultado")
- Resultado2.innerHTML = decifrar.value
- }
+ //*const Resultado1 = document.getElementById("Resultado")
+ //*Resultado1.innerHTML = cifrar.value
+
+ //*const Resultado2 = document.getElementById("Resultado")
+ //*Resultado2.innerHTML = decifrar.value
+ //*}
  
 
 
